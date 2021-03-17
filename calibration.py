1d500b05faa5ecb592207847f5543b2a970edcff
Orestis <orestispanago@gmail.com>
2021-03-17 14:29:17 +0200

    Added cmp11 10-min timeseries

--- calibration.py
+++ calibration.py
@@ -7,16 +7,24 @@ vaisala = pd.read_csv("vaisala.txt",
                       parse_dates={'date': ['day', 'time']},
                       index_col="date",
                       sep='\t')
+vaisala.index = vaisala.index.round("1min")
+vaisala = vaisala.tz_localize("EET").tz_convert('UTC')
 
 
-reference = pd.read_csv("reference.txt", sep=" ", 
-                        names=["day", "time", "a", "b", "c", "d", "e", "f", "g"],
-                        parse_dates={'date': ['day', 'time']},
-                        index_col="date")
+cmp11 = pd.read_csv("cmp11.txt", 
+                     names=["TIMESTAMP","RECORD","GHI_final","DNI_final",
+                            "DHI_final","Sensor_Temp_Avg","LoggerVoltage_Min",
+                            "LoggerTemp_C_Max","Error_Max"],
+                     parse_dates=True,
+                     index_col="TIMESTAMP")
+cmp11 = cmp11.tz_localize("UTC")
+
+merged = pd.concat([vaisala, cmp11], axis=1)
 
 
 # plt.plot(vaisala.index ,vaisala["average"], label="vaisala")
 vaisala["average"].plot(label="vaisala")
+cmp11["GHI_final"].plot(label="cmp11")
 # plt.xticks(rotation=45)
 plt.legend()
 plt.show()
